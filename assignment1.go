beae5f707b124144e82a3958cd7fc7e5996c28b7
md-arif-rzp <mohammad.arif@razorpay.com>
2021-07-20 14:12:28 +0530

    random

--- assignment1.go
+++ assignment1.go
@@ -7,12 +7,18 @@ import(
 
 func add(a ,b string ) (string,string ) {
 	return b,a}
+func ruuc(x int ) int {
+	if x==0 {return 1}
 
+	return x * ruuc(x-1)
+}
 
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(100))
 	x,y :=add("hello","world")
 	fmt.Println(x,y)
+
+	fmt.Println(ruuc(7))
 
 }
