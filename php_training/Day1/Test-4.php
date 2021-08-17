52c058bc9c01094ec01a7d87ea82e2c18d8b58f3
md-arif-rzp <mohammad.arif@razorpay.com>
2021-08-17 21:00:09 +0530

    test4 clean code

--- php_training/Day1/Test-4.php
+++ php_training/Day1/Test-4.php
@@ -12,18 +12,34 @@ $age=array();
 $city=array();
 
 
-foreach($obj->{'players'} as $value){
-        array_push($name,$value->{'name'});
-        array_push($age,$value->{'age'});
-        array_push($city,$value->{'address'}->{'city'});
+foreach($obj->{'players'} as $player){
+        array_push($name,$player->name);
+        array_push($age,$player->age);
+        array_push($city,$player->address->city);
 
 }
 
-
-echo "Name: " . json_encode($name);
+echo "Names: " . json_encode($name);
 echo "Age: " . json_encode($age);
 echo "City: " . json_encode($city);
 
+ // print only unique names
+ $unique_names = array_unique($name);
+ echo "Names: " . json_encode($unique_names);
+
+ //max age 
+ $maxAge = 0;
+    $maxAgePlayers = [];
+    foreach ($obj->{'players'} as $player) {
+        $maxAge = max($maxAge, $player->age);
+    }
+    foreach ($obj->{'players'} as $player) {
+        if ($player->age == $maxAge) {
+            array_push($maxAgePlayers, $player->name);
+        }
+    }
+
+echo "Names: " . json_encode($maxAgePlayers);
 
 
 ?>
