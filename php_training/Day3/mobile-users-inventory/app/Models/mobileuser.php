70e0f6d2333141dc8b6eac8d584eae8a13238671
md-arif-rzp <mohammad.arif@razorpay.com>
2021-08-20 16:48:52 +0530

    minor update

--- php_training/Day3/mobile-users-inventory/app/Models/mobileuser.php
+++ php_training/Day3/mobile-users-inventory/app/Models/mobileuser.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class mobileuser extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'UserNname',
+        'UserEmail',
+        'MobileNo'
+    ];
     public $timestamps=false;
     protected $primaryKey='MobileNo';
     public $incrementing = false;
